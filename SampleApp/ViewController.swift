d9a31bb730976e46bd5e31fb93cdace7c8476407
parvind bhatt <parvindbhatt@Nagarros-MacBook-Pro-2.local>
2019-02-12 16:47:32 +0530

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by parvind bhatt on 12/02/19.
+//  Copyright © 2019 parvind bhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
